2817c807ecf27baa78593ae0ebebdc8bfa2cd687
jonesna213 <navyjones9@gmail.com>
2023-03-23 16:58:22 -0500

    Created Basket Models

--- MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -10,6 +10,8 @@ namespace MyShop.DataAccess.SQL {
     public class DataContext : DbContext {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
 
         public DataContext() 
             :base("DefaultConnection") { 
